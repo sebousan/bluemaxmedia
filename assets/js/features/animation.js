016bb5614fcccb77897b92f15349f664928eb85d
sebousan <sebousan@gmail.com>
2026-02-02 16:44:40 +0100

    feature: hero animation

--- assets/js/features/animation.js
+++ assets/js/features/animation.js
@@ -1,4 +1,10 @@
 import scrollspy from '../utils/scrollspy';
+import Splitting from 'splitting';
+
+const splitElements = document.querySelectorAll('.js-split');
+splitElements.forEach(element => {
+  Splitting({ target: element, by: 'lines' });
+});
 
 const elements = document.querySelectorAll('[data-anim]');
 window.addEventListener('load', function () {
